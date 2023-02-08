aeddb69cea6f9cde6ef8647d4d0d558306b18157
Dane Dalton <danezdalton1@gmail.com>
2023-02-08 17:02:00 -0600

    Get prepped for the schedule section

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
+
 import { classNames } from '../util/class-names'
+
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai'
 
 import {
@@ -9,7 +11,6 @@ import {
   endOfWeek,
   format,
   isToday,
-  startOfMonth,
   startOfWeek,
   startOfToday,
   isEqual,
@@ -24,9 +25,9 @@ function Calendar() {
   let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
   let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
 
-  let newDays = eachDayOfInterval({ 
-    start: startOfWeek(startOfMonth(firstDayCurrentMonth)), 
-    end: endOfWeek(endOfMonth(firstDayCurrentMonth)) 
+  let daysOfMonth = eachDayOfInterval({
+    start: startOfWeek(firstDayCurrentMonth),
+    end: endOfWeek(endOfMonth(firstDayCurrentMonth))
   })
 
   function previousMonth() {
@@ -40,79 +41,96 @@ function Calendar() {
   }
 
   return (
-    <div>
-      <div>
-        <div className='w-96 flex items-center'>
-          <h2 className='flex-auto font-semibold text-gray-900'>
-            {format(firstDayCurrentMonth, 'MMMM yyyy')}
-          </h2>
-          <button
-            onClick={previousMonth}
-          >
-            <span className='sr-only'>Previous month</span>
-            <AiOutlineLeft className='w-5 h-5' aria-hidden='true' />
-          </button>
-          <button
-            onClick={nextMonth}
-          >
-            <span className='sr-only'>Next month</span>
-            <AiOutlineRight className='w-5 h-5' aria-hidden='true' />
-          </button>
-        </div>
-        <div>
-          <div className='w-96 grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
-            <div>S</div>
-            <div>M</div>
-            <div>T</div>
-            <div>W</div>
-            <div>T</div>
-            <div>F</div>
-            <div>S</div>
+    <div className='pt-16'>
+      <div className='max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6'>
+        <div className='md:grid md:grid-cols-2 md:divide-x md:divide-gray-200'>
+          <div className='md:pr-14'>
+            <div className='flex items-center'>
+              <h2 className='flex-auto font-semibold text-gray-900'>
+                {format(firstDayCurrentMonth, 'MMMM yyyy')}
+              </h2>
+              <button
+                type='button'
+                onClick={previousMonth}
+                className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
+              >
+                <span className='sr-only'>Previous month</span>
+                <AiOutlineLeft className='w-5 h-5' aria-hidden='true' />
+              </button>
+              <button
+                type='button'
+                onClick={nextMonth}
+                className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
+              >
+                <span className='sr-only'>Next month</span>
+                <AiOutlineRight className='w-5 h-5' aria-hidden='true' />
+              </button>
+            </div>
+            <div className='grid grid-cols-7 mt-8 text-xs leading-6 text-center text-gray-500'>
+              <div>S</div>
+              <div>M</div>
+              <div>T</div>
+              <div>W</div>
+              <div>T</div>
+              <div>F</div>
+              <div>S</div>
+            </div>
+            <div className='mt-2 text-sm grid grid-cols-7'>
+              {daysOfMonth.map((day, dayIdx) => (
+                <div
+                  key={day.toString()}
+                  className={classNames(
+                    dayIdx > 6 && 'border-t border-gray-200',
+                    'py-1.5'
+                  )}
+                >
+                  <button
+                    type='button'
+                    onClick={() => setSelectedDay(day)}
+                    className={classNames(
+                      isEqual(day, selectedDay) && 'text-white',
+                      !isEqual(day, selectedDay) &&
+                      isToday(day) &&
+                      'text-red-500',
+                      !isEqual(day, selectedDay) &&
+                      !isToday(day) &&
+                      isSameMonth(day, firstDayCurrentMonth) &&
+                      'text-gray-900',
+                      !isEqual(day, selectedDay) &&
+                      !isToday(day) &&
+                      !isSameMonth(day, firstDayCurrentMonth) &&
+                      'text-gray-400',
+                      isEqual(day, selectedDay) &&
+                      isToday(day) &&
+                      'bg-red-500',
+                      isEqual(day, selectedDay) &&
+                      !isToday(day) &&
+                      'bg-gray-900',
+                      !isEqual(day, selectedDay) && 'hover:bg-gray-200',
+                      (isEqual(day, selectedDay) || isToday(day)) &&
+                      'font-semibold',
+                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
+                    )}
+                  >
+                    <time dateTime={format(day, 'yyyy-MM-dd')}>
+                      {format(day, 'd')}
+                    </time>
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
+          <section className='mt-12 md:mt-0 md:pl-14'>
+            <h2 className='font-semibold text-gray-900'>
+              Schedule for <time dateTime='2023-02-08'>Feb 8, 2023</time>
+            </h2>
+            <ol>
+              {/* Map schedule here */}
+            </ol>
+          </section>
         </div>
       </div>
 
-      <div className='w-96 h-96 mt-2 text-sm grid grid-cols-7'>
-        {newDays.map((day, dayIdx) => (
-          <div
-            key={day.toString()}
-            className='border-t border-gray-200'
-          >
-            <button
-              type='button'
-              onClick={() => setSelectedDay(day)}
-              className={classNames(
-                isEqual(day, selectedDay) && 'text-white',
-                !isEqual(day, selectedDay) &&
-                  isToday(day) &&
-                  'text-red-500',
-                !isEqual(day, selectedDay) &&
-                  !isToday(day) &&
-                  isSameMonth(day, firstDayCurrentMonth) &&
-                  'text-gray-900',
-                !isEqual(day, selectedDay) &&
-                  !isToday(day) &&
-                  !isSameMonth(day, firstDayCurrentMonth) &&
-                  'text-gray-400',
-                isEqual(day, selectedDay) && 
-                  isToday(day) && 
-                  'bg-red-500',
-                isEqual(day, selectedDay) &&
-                  !isToday(day) &&
-                  'bg-gray-900',
-                !isEqual(day, selectedDay) && 'hover:bg-gray-200',
-                (isEqual(day, selectedDay) || isToday(day)) &&
-                'font-semibold',
-                'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
-              )}
-            >
-              <time dateTime={format(day, 'yyyy-MM-dd')}>
-                {format(day, 'd')}
-              </time>
-            </button>
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
